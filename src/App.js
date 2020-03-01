7934b411343d93c520f4239120015f77b9a31f9c
Luke Wyman <lukewyman@gmail.com>
2020-02-29 19:54:55 -0700

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hi there!
+        I was changed on the Feature branch
       </header>
     </div>
   );
